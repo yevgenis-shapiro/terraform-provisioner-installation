01b79229afcf23fa89f7d2ea08a290344219d484
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-05-02 22:13:23 +0300

    update

---
+++ main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "tf-sec-gr" {
+  name        = "tf-provisioner-sg"
+  description = "Security group for terraform provisioner"
+  tags = {
+    Name = "tf-provisioner-sg"
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = -1
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "instance" {
+  ami             = var.ec2_ami
+  instance_type   = var.ec2_type
+  key_name        = var.key_name
+  #vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  #vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  security_groups = ["tf-provisioner-sg"]
+
+  tags = {
+    Name = "terraform-instance-with-provisioner"
+  }
+
+  provisioner "local-exec" {
+    command = "echo http://${self.public_ip} > public_ip.txt"
+  }
+
+  connection {
+    host        = self.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd",
+      "sudo systemctl enable httpd",
+      "sudo systemctl start httpd"
+    ]
+  }
+
+  provisioner "file" {
+    content     = self.public_ip
+    destination = "/home/ec2-user/my_public_ip.txt"
+  }
+}
