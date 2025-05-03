c27e47a79eaca82dd23d3f2d48a72e293a7905a6
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-05-03 22:58:40 +0300

    update

--- main.tf
+++ main.tf
@@ -37,10 +37,17 @@ resource "aws_security_group" "tf-sec-gr" {
   }
 
   ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["84.228.99.5/32"]
   }
 
   egress {
